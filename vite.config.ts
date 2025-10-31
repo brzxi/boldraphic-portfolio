1f5399ebd1170b484c540e74d15f4ec3f16f90d1
brzxi <basel1424221@gmail.com>
2025-11-01 02:09:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -42,3 +42,9 @@ export default defineConfig({
     },
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
